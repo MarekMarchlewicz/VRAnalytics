6ec38f98d1dd86d89f563dbb7eb7acf737a293e5
Marek <marek.marchlewicz@gmail.com>
2017-04-28 14:19:34 +0800

    Target Platform - > Android Added mouse following script

---
+++ Assets/Scripts/MouseFollower.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Based on GvrViewer from Google
+/// </summary>
+public class MouseFollower : MonoBehaviour
+{
+    [SerializeField, Range(0, 1)]
+    private float neckScaleModel;
+
+    private static readonly Vector3 neckOffset = new Vector3(0, 0.075f, 0.08f);
+
+    private Quaternion initialRotation = Quaternion.identity;
+
+    private static float mouseX = 0;
+    private static float mouseY = 0;
+    private static float mouseZ = 0;
+
+    private void Update()
+    {
+        UpdateRotation();
+    }
+
+    public void UpdateRotation()
+    {
+        Quaternion rot;
+
+        bool rolled = false;
+
+        if (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
+        {
+            mouseX += Input.GetAxis("Mouse X") * 5;
+            if (mouseX <= -180)
+            {
+                mouseX += 360;
+            }
+            else if (mouseX > 180)
+            {
+                mouseX -= 360;
+            }
+            mouseY -= Input.GetAxis("Mouse Y") * 2.4f;
+            mouseY = Mathf.Clamp(mouseY, -85, 85);
+        }
+        else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            rolled = true;
+            mouseZ += Input.GetAxis("Mouse X") * 5;
+            mouseZ = Mathf.Clamp(mouseZ, -85, 85);
+        }
+        if (!rolled)
+        {
+            mouseZ = Mathf.Lerp(mouseZ, 0, Time.deltaTime / (Time.deltaTime + 0.1f));
+        }
+        rot = Quaternion.Euler(mouseY, mouseX, mouseZ);
+        var neck = (rot * neckOffset - neckOffset.y * Vector3.up) * neckScaleModel;
+
+        transform.localPosition = neck;
+        transform.localRotation = rot;
+    }
+}
